596f41e5f81f48dae1f3af0dae0eff32e984178c
Rebecca Piper <piper.rebecca93@gmail.com>
2016-02-19 11:28:17 +0000

    Continued to edit CSS

--- interface.js
+++ interface.js
@@ -2,32 +2,32 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $('.temperature-upButton').on('click', function(){
+  $('#temperature-upButton').on('click', function(){
     thermostat.upButton();
     updateTemperature();
   });
 
-  $('.temperature-downButton').on('click', function(){
+  $('#temperature-downButton').on('click', function(){
     thermostat.downButton();
     updateTemperature();
   });
 
-  $('.temperature-resetTemp').on('click', function(){
+  $('#temperature-resetTemp').on('click', function(){
     thermostat.resetTemp();
     updateTemperature();
   });
 
-  $('.power-saving-mode').on('click', function(){
+  $('#power-saving-mode').on('click', function(){
     thermostat.changeMode();
     if (thermostat.powerSM === false){
-      $('.power-saving-status').text("off");
+      $('#power-saving-status').text("Off");
     }
     else {
-      $('.power-saving-status').text("on");
+      $('#power-saving-status').text("On");
     };
   });
 
-  $('.local-city').change(function(){
+  $('#local-city').change(function(){
     var city = $('.local-city').val();
     displayWeather(city);
   });
@@ -37,12 +37,12 @@ $(document).ready(function() {
     var token = '&appid=f83f44a7a0afa1876ffd608629d71ef1';
     var units = '&units=metric';
     $.get(url + token + units, function(data){
-      $('.local-temperature').text(data.main.temp + " DEGREES");
+      $('#local-temperature').text("outside " + data.main.temp + " degrees");
     });
   };
 
   function updateTemperature(){
-    $('.temperature').text(thermostat.currentTemp() + " DEGREES");
+    $('#temperature').text(thermostat.currentTemp() + " DEGREES");
     $('*').css("background-color", thermostat.displayTemp());
   };
 });
